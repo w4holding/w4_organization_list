38a9c197c9e5366b395557220fad11aba04c21ba
Jaume Presas <jpresas@w-4.com>
2023-03-20 17:12:34 +0100

    [[TASK] Removed fontawesome icons

--- Configuration/Icons.php
+++ Configuration/Icons.php
@@ -9,4 +9,40 @@ return [
         'provider' => \TYPO3\CMS\Core\Imaging\IconProvider\SvgIconProvider::class,
         'source' => 'EXT:w4_organization_list/Resources/Public/Icons/user_plugin_club.svg'
     ],
+    'ext-w4organizationlist-wizard-icon' => [
+        'provider' => \TYPO3\CMS\Core\Imaging\IconProvider\SvgIconProvider::class,
+        'source' => 'EXT:w4_organization_list/Resources/Public/Icons/user_plugin_club.svg'
+    ],
+    'ext-w4organizationlist-edit-icon' => [
+        'provider' => \TYPO3\CMS\Core\Imaging\IconProvider\SvgIconProvider::class,
+        'source' => 'EXT:w4_organization_list/Resources/Public/Icons/user_plugin_club.svg'
+    ],
+    'ext-w4organizationlist-delete-icon' => [
+        'provider' => \TYPO3\CMS\Core\Imaging\IconProvider\SvgIconProvider::class,
+        'source' => 'EXT:w4_organization_list/Resources/Public/Icons/user_plugin_club.svg'
+    ],
+    'ext-w4organizationlist-phone-icon' => [
+        'provider' => \TYPO3\CMS\Core\Imaging\IconProvider\SvgIconProvider::class,
+        'source' => 'EXT:w4_organization_list/Resources/Public/Icons/user_plugin_club.svg'
+    ],
+    'ext-w4organizationlist-fax-icon' => [
+        'provider' => \TYPO3\CMS\Core\Imaging\IconProvider\SvgIconProvider::class,
+        'source' => 'EXT:w4_organization_list/Resources/Public/Icons/user_plugin_club.svg'
+    ],
+    'filemetadata-status-1' => [
+        'provider' => \TYPO3\CMS\Core\Imaging\IconProvider\SvgIconProvider::class,
+        'source' => 'EXT:w4_organization_list/Resources/Public/Icons/user_plugin_club.svg'
+    ],
+    'filemetadata-status-2' => [
+        'provider' => \TYPO3\CMS\Core\Imaging\IconProvider\SvgIconProvider::class,
+        'source' => 'EXT:w4_organization_list/Resources/Public/Icons/user_plugin_club.svg'
+    ],
+    'filemetadata-status-3' => [
+        'provider' => \TYPO3\CMS\Core\Imaging\IconProvider\SvgIconProvider::class,
+        'source' => 'EXT:w4_organization_list/Resources/Public/Icons/user_plugin_club.svg'
+    ],
+    'w4_organization_list-plugin-club' => [
+        'provider' => \TYPO3\CMS\Core\Imaging\IconProvider\SvgIconProvider::class,
+        'source' => 'EXT:w4_organization_list/Resources/Public/Icons/user_plugin_club.svg'
+    ],
 ];
--- ext_localconf.php
+++ ext_localconf.php
@@ -3,52 +3,6 @@ defined('TYPO3') || die();
 
 (static function() {
 
-    $icons = [
-        'ext-w4organizationlist-wizard-icon' => [
-            'name' => 'address-card'
-        ],
-        'ext-w4organizationlist-edit-icon' => [
-            'name' => 'pencil-square-o'
-        ],
-        'ext-w4organizationlist-delete-icon' => [
-            'name' => 'trash-o'
-        ],
-        'ext-w4organizationlist-phone-icon' => [
-            'name' => 'phone'
-        ],
-        'ext-w4organizationlist-fax-icon' => [
-            'name' => 'fax'
-        ],
-
-        // status icons for "Baugesuche"
-        'filemetadata-status-1' => [
-            'name' => 'check-circle'
-        ],
-        'filemetadata-status-2' => [
-            'name' => 'times-circle' // declined/abgelehnt override instead of add 4th status with icon
-        ],
-        'filemetadata-status-3' => [
-            'name' => 'exclamation-circle'
-        ],
-        'w4_organization_list-plugin-club' => [
-            'name' => 'times-circle'
-        ],
-    ];
-
-    $iconRegistry = \TYPO3\CMS\Core\Utility\GeneralUtility::makeInstance(
-        \TYPO3\CMS\Core\Imaging\IconRegistry::class
-    );
-
-    foreach ($icons as $identifier => $icon) {
-
-        $iconRegistry->registerIcon(
-            $identifier,
-            \TYPO3\CMS\Core\Imaging\IconProvider\FontawesomeIconProvider::class,
-            $icon
-        );
-
-    }
-
     \TYPO3\CMS\Extbase\Utility\ExtensionUtility::configurePlugin(
         'W4OrganizationList',
         'Club',
